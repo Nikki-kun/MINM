fa1f2787af23930eafe441eb327f1e45ca521d88
Nikki-kun <arikovicaroslav4@gmail.com>
2025-10-10 22:27:55 +0300

    add qt in project

--- src/init.cpp
+++ src/init.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
-#include "core/message.h"
-using namespace std;
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
 
-int main(){
-    Message i = Message();
-    return 0;
+int main(int argc, char *argv[])
+{
+    	QApplication app(argc, argv);
+
+    	QMainWindow mainWindow;
+    	mainWindow.setWindowTitle("My Qt Application");
+    	mainWindow.resize(800, 600);
+
+    	QWidget *centralWidget = new QWidget(&mainWindow);
+	QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+	QLabel *label = new QLabel("Hello, Qt!", centralWidget);
+	label->setAlignment(Qt::AlignCenter);
+	layout->addWidget(label);
+
+    	mainWindow.setCentralWidget(centralWidget);
+    	mainWindow.show();
+
+	return app.exec();
 }
